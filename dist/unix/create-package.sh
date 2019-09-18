c73b24e12d70ed2cea61da6c109d9897bd8a8d2a
Shatur95 <genaloner@gmail.com>
2019-09-18 20:14:09 +0300

    Refactor install script

--- dist/unix/create-package.sh
+++ dist/unix/create-package.sh
@@ -1,66 +1,60 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
 . /etc/os-release
 
 cleanDir()
 {
-		make clean
-        rm crow
-        rm .qmake.stash
-        rm Makefile
+    make clean
+    rm crow
+    rm .qmake.stash
+    rm Makefile
 }
 
 case $ID in
-	"arch" | "parabola" | "manjarolinux" )
+    "arch" | "parabola" | "manjarolinux" )
         cd archlinux
-	
+    
         makepkg
         rm -r pkg
         rm -r src
         cd ../../..
         cleanDir
-	
+    
         echo -e "\x1b[1;32mNow you can install Crow Translate by running the following commands:\x1b[0m"
         echo -e "\x1b[1;37mcd archlinux\x1b[0m"
         echo -e "\x1b[1;37msudo pacman -U crow-git-*.pkg.tar.xz\x1b[0m"
     ;;
-	
-	"ubuntu" | "linuxmint" | "elementary" | "debian" | "devuan" )
-		if [ -z $(which debuild) ]; then
-			echo "Package devsripts REQUIRED for building:"
-			sudo apt install devscripts qt5-default qt5-qmake libqt5x11extras5-dev qttools5-dev-tools qtmultimedia5-dev qtbase5-dev qtbase5-dev-tools
-			
-			[ "$?" -ne 0 ] && echo "You can still install it by yourself." && exit 0
-		fi
-		
-		cd ../..
-		VER=$(awk 'NR==1 {print $2}' dist/unix/debian/changelog | sed 's/-[1-9]//g;s/[()]//g')
-		tar czfv crow-translate_$VER.orig.tar.gz *
-		mv crow-translate_*.orig.tar.gz ..
-		cp -r dist/unix/debian .
-		debuild -i -us -uc
-		rm -rf debian
-		cleanDir
-		
+    
+    "ubuntu" | "linuxmint" | "elementary" | "debian" | "devuan" )
+        echo "Installing dependencies..."
+        sudo apt install devscripts qt5-default qt5-qmake libqt5x11extras5-dev qttools5-dev-tools qtmultimedia5-dev qtbase5-dev qtbase5-dev-tools
+        
+        cd ../..
+        cp -r dist/unix/debian .
+        VER=$(awk 'NR==1 {print $2}' debian/changelog | sed 's/-[1-9]//g;s/[()]//g')
+        tar czfv ../crow-translate_$VER.orig.tar.gz .github *
+        debuild -i -us -uc
+        rm -r debian
+        cleanDir
+        
         echo -e "\x1b[1;32mNow you can install Crow Translate by running the following commands:\x1b[0m"
         echo -e "\x1b[1;37mcd ../../..\x1b[0m"
         echo -e "\x1b[1;37msudo dpkg -i crow-translate_$VER-1_amd64.deb\x1b[0m"
         echo -e "\x1b[1;37msudo apt install -f\x1b[0m"
-	;;
-	
-	"fedora" )
-	echo -e "\x1b[1;31mSorry, package generation for your distribution temporally unsupported\x1b[0m"
-	;;
-	
-	*)
-		echo -e "\x1b[1;37mCan't determine your distribution. The project will be compiled and installed in the \"install\" folder.\x1b[0m"
-		
-		export MAKEFLAGS="-j$(nproc)"
-		qmake
-		make
-		mkdir ../install
-		PREFIX=../install make install
-		cleanDir
-		echo -e "\x1b[1;32mCrow Translate was successfully installed in the \"install\" folder\x1b[0m"
-	;;
+    ;;
+    
+    "fedora" )
+    echo -e "\x1b[1;31mSorry, package generation for your distribution temporally unsupported\x1b[0m"
+    ;;
+    
+    *)
+        echo -e "\x1b[1;37mCan't determine your distribution. The project will be compiled and installed in the \"install\" folder.\x1b[0m"
+        
+        qmake
+        make
+        mkdir ../install
+        PREFIX=../install make -j$(nproc) install
+        cleanDir
+        echo -e "\x1b[1;32mCrow Translate was successfully installed in the \"install\" folder\x1b[0m"
+    ;;
 esac
