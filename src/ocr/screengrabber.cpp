20399b369ecb8d7c0aee42f6236a0d190f6e1a2a
Shatur95 <genaloner@gmail.com>
2020-10-17 21:17:34 +0300

    Update ScreenGrabber copyright

--- src/ocr/screengrabber.cpp
+++ src/ocr/screengrabber.cpp
@@ -1,20 +1,22 @@
 /*
- *  Copyright (C) 2018 Ambareesh "Amby" Balaji <ambareeshbalaji@gmail.com>
+ *  Copyright © 2018 Ambareesh "Amby" Balaji <ambareeshbalaji@gmail.com>
+ *  Copyright © 2018-2020 Hennadii Chernyshchyk <genaloner@gmail.com>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  This file is part of Crow Translate.
+ *
+ *  Crow Translate is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  Crow Translate is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ *  You should have received a get of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include "screengrabber.h"
