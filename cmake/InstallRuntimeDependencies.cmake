10999e54e7b7831b82fc7deb7f005b58fdbcbf82
Shatur95 <genaloner@gmail.com>
2020-10-21 02:28:24 +0300

    Windows: Add module to install all necessary DLLs For use with VCPKG. The previous windeployqt approach did not support Tesseract.

---
+++ cmake/InstallRuntimeDependencies.cmake
@@ -0,0 +1,14 @@
+# Installs all required DLLs and Qt translations
+# Only works for VCPKG
+function(install_runtime_dependencies TARGET)
+    if(VCPKG_TOOLCHAIN AND _VCPKG_TARGET_TRIPLET_PLAT STREQUAL windows)
+        install(CODE "execute_process(COMMAND 
+            powershell -noprofile -executionpolicy Bypass -file \"${_VCPKG_TOOLCHAIN_DIR}/msbuild/applocal.ps1\"
+            -targetBinary \"\${CMAKE_INSTALL_PREFIX}/$<TARGET_FILE_NAME:${TARGET}>\"
+            -installedDir \"${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}$<$<CONFIG:Debug>:/debug>/bin\"
+            -OutVariable out)"
+        )
+        file(GLOB QT_TRANSLATIONS ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/qt5/translations/qt_[^help]*.qm)
+        install(FILES ${QT_TRANSLATIONS} DESTINATION translations)
+    endif()
+endfunction()
