af485932682f23826f1b4cae5221dc9771609bf0
Shatur95 <genaloner@gmail.com>
2020-12-19 16:53:54 +0200

    Update QOnlineTranslator library

--- cmake/ExternalLibraries.cmake
+++ cmake/ExternalLibraries.cmake
@@ -19,7 +19,7 @@ FetchContent_Declare(QHotkey
 
 FetchContent_Declare(QOnlineTranslator
     GIT_REPOSITORY https://github.com/crow-translate/QOnlineTranslator
-    GIT_TAG 297befe8414b6b49f27b0b12532bfd71ab7ecf5b
+    GIT_TAG 1.4.1
 )
 
 FetchContent_MakeAvailable(SingleApplication QTaskbarControl QHotkey QOnlineTranslator)
