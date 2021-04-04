4f8a1f41347442671ea07153dc00416476f39ec2
Shatur95 <genaloner@gmail.com>
2021-04-04 20:19:05 +0300

    Add AppImage as CPack external generator, closes #138

---
+++ cmake/AppImage.cmake
@@ -0,0 +1,6 @@
+find_program(LINUXDEPLOY_EXECUTABLE
+  NAMES linuxdeploy linuxdeploy-x86_64.AppImage
+  HINTS ${CPACK_PACKAGE_DIRECTORY} ${CPACK_LINUXDEPLOY_ROOT}
+)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E env EXTRA_QT_PLUGINS=multimediawidgets\;svg VERSION=${CPACK_PACKAGE_VERSION} ${LINUXDEPLOY_EXECUTABLE} --appdir=${CPACK_TEMPORARY_DIRECTORY} --plugin=qt --output=appimage)
---
+++ cmake/CPackProjectConfig.cmake
@@ -0,0 +1,3 @@
+if(CPACK_GENERATOR STREQUAL External)
+    set(CPACK_PACKAGING_INSTALL_PREFIX /usr)
+endif()
