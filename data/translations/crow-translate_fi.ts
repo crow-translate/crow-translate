a032f80e5acd2f7c2940fb1ea660bec5d01df643
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-05-08 10:12:02 +0300

    Update Finnish localization from Crowdin

--- data/translations/crow-translate_fi.ts
+++ data/translations/crow-translate_fi.ts
@@ -1227,7 +1227,7 @@
       <location filename="../../src/settings/settingsdialog.ui" line="456"/>
       <location filename="../../src/settings/settingsdialog.ui" line="480"/>
       <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Languages text on buttons&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Kielten teksti painikkeissa&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Kielien teksti painikkeissa&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
       <location filename="../../src/settings/settingsdialog.ui" line="500"/>
