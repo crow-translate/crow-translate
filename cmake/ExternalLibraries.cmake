639289cdfcfc6e486f9ddcc83396a4b2f142aeb3
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-03-09 18:23:45 +0200

    Bump QOnlineTranslator to 1.5.3
    * Fix Lingva Chinese codes.

--- cmake/ExternalLibraries.cmake
+++ cmake/ExternalLibraries.cmake
@@ -19,7 +19,7 @@ FetchContent_Declare(QHotkey
 
 FetchContent_Declare(QOnlineTranslator
     GIT_REPOSITORY https://github.com/crow-translate/QOnlineTranslator
-    GIT_TAG 1.5.2
+    GIT_TAG 1.5.3
 )
 
 FetchContent_Declare(CircleFlags
