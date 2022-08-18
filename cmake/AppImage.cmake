0de7e4671cf1b50348533ebb9cd4405900a7051f
eljamm <83901271+eljamm@users.noreply.github.com>
2022-08-18 13:37:10 +0100

    Make AppImages updateable

--- cmake/AppImage.cmake
+++ cmake/AppImage.cmake
@@ -4,5 +4,6 @@ find_program(LINUXDEPLOY_EXECUTABLE
 )
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E env EXTRA_QT_PLUGINS=multimediawidgets\;svg VERSION=${CPACK_PACKAGE_VERSION}
+  UPDATE_INFORMATION=gh-releases-zsync|crow-translate|crow-translate|latest|Crow_Translate-*-x86_64.AppImage.zsync
   ${LINUXDEPLOY_EXECUTABLE} --appdir=${CPACK_TEMPORARY_DIRECTORY} --plugin=qt --plugin=gstreamer --output=appimage
 )
