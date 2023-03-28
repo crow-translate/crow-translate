fc995b676695cd5f2665759231fcba7d649bef14
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-03-28 19:08:43 +0300

    Update localization strings

--- data/translations/crow-translate.ts
+++ data/translations/crow-translate.ts
@@ -312,7 +312,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ocr/ocr.cpp" line="147"/>
+        <location filename="../../src/ocr/ocr.cpp" line="148"/>
         <source>%1 is not a valid Tesseract parameter name.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1032,46 +1032,46 @@
     <message>
         <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1249"/>
         <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1258"/>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1341"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1346"/>
         <source>Error: Engine systems have detected suspicious traffic from your computer network. Please try your request again later.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1270"/>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1421"/>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1614"/>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1708"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1426"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1626"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1720"/>
         <source>Error: Unable to parse autodetected language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1348"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1353"/>
         <source>Error: Unable to find Yandex SID in web version.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1355"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1360"/>
         <source>Error: Unable to extract Yandex SID from web version.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1535"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1542"/>
         <source>Error: Unable to find Bing credentials in web version.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1542"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1549"/>
         <source>Error: Unable to extract Bing key from web version.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1550"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1557"/>
         <source>Error: Unable to extract Bing token from web version.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1558"/>
-        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1566"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1569"/>
+        <location filename="../../src/qonlinetranslator/src/qonlinetranslator.cpp" line="1578"/>
         <source>Error: Unable to extract additional Bing information from web version.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1171,8 +1171,8 @@
     </message>
     <message>
         <location filename="../../src/settings/settingsdialog.cpp" line="75"/>
-        <location filename="../../src/settings/settingsdialog.cpp" line="100"/>
-        <location filename="../../src/settings/settingsdialog.cpp" line="101"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="103"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="104"/>
         <source>&lt;System language&gt;</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1804,7 +1804,7 @@
     </message>
     <message>
         <location filename="../../src/settings/settingsdialog.ui" line="1333"/>
-        <location filename="../../src/settings/settingsdialog.cpp" line="405"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="408"/>
         <source>Default region</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1834,11 +1834,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.ui" line="1586"/>
-        <source>Enable global shortcucts</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../../src/settings/settingsdialog.ui" line="1615"/>
         <source>Shortcut</source>
         <translation type="unfinished"></translation>
@@ -1856,6 +1851,11 @@
     <message>
         <location filename="../../src/settings/settingsdialog.ui" line="1534"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Proxy password for authentication&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/settingsdialog.ui" line="1586"/>
+        <source>Enable global shortcuts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -1934,118 +1934,118 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="146"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="149"/>
         <source>Updates</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="153"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="156"/>
         <source>Check for updates:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="157"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="160"/>
         <source>Every day</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="158"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="161"/>
         <source>Every week</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="159"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="162"/>
         <source>Every month</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/settings/settingsdialog.ui" line="1077"/>
-        <location filename="../../src/settings/settingsdialog.cpp" line="160"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="163"/>
         <source>Never</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="136"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="139"/>
         <source>Icons:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="164"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="167"/>
         <source>Check now</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="165"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="168"/>
         <source>Check for updates now</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="178"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="181"/>
         <source>Happy New Year!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="192"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="195"/>
         <source>The OCR parameter fields can not be empty.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="193"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="196"/>
         <source>Do you want to discard the invalid parameters?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="343"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="346"/>
         <source>Select icon</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="343"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="346"/>
         <source>Images (*.png *.ico *.svg *.jpg);;All files()</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="356"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="359"/>
         <source>Select OCR languages path</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="408"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="411"/>
         <source>Mandarin (China)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="716"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="719"/>
         <source>Nothing to play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="716"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="719"/>
         <source>Playback text is empty</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="726"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="729"/>
         <source>Unable to detect language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="483"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="486"/>
         <source>Checking for updates...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="505"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="508"/>
         <source>Update available!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="511"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="514"/>
         <source>No updates available.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/settings/settingsdialog.cpp" line="610"/>
+        <location filename="../../src/settings/settingsdialog.cpp" line="613"/>
         <source>Back</source>
         <translation type="unfinished"></translation>
     </message>
