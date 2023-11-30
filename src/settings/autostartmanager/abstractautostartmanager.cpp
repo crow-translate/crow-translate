8cdf145fce92588ea721a52873deb53ab2a4e726
Emanuel Haupt <ehaupt@critical.ch>
2023-11-30 19:11:06 +0100

    Fix settings dialog on FreeBSD
    Reference: https://github.com/crow-translate/crow-translate/issues/621

--- src/settings/autostartmanager/abstractautostartmanager.cpp
+++ src/settings/autostartmanager/abstractautostartmanager.cpp
@@ -18,7 +18,7 @@
  */
 
 #include "abstractautostartmanager.h"
-#if defined(Q_OS_LINUX)
+#if defined(Q_OS_UNIX)
 #include "portalautostartmanager.h"
 #include "unixautostartmanager.h"
 #elif defined(Q_OS_DARWIN)
@@ -36,7 +36,7 @@ AbstractAutostartManager::AbstractAutostartManager(QObject *parent)
 
 AbstractAutostartManager *AbstractAutostartManager::createAutostartManager(QObject *parent)
 {
-#if defined(Q_OS_LINUX)
+#if defined(Q_OS_UNIX)
     if (PortalAutostartManager::isAvailable())
         return new PortalAutostartManager(parent);
     return new UnixAutostartManager(parent);
